c43418727ce3d553ea09005b5dd76130de9544b7
Unknown <niki.frendo@gmail.com>
2018-04-03 10:26:25 +0200

    ** UnTestedChanges ** Producer main loop changes
    - Input video file is converted to FLAC file (1-channel, 48000Hz)
    - Resulting FLAC file is uploaded to Google Storage and deleted from disk

--- src/kafka/producer_runner.py
+++ src/kafka/producer_runner.py
@@ -5,10 +5,11 @@ from src.recording.recording_interface import RecordingInterface
 from src.constants import path_consts as pc
 from src.kafka.producer import Producer
 from src.object_definitions.stream_object import StreamObject
+from src.speech_recognition.BDAGoogleStorage import BDAGoogleStorageConvertUpload
 import time
 """
 This script is intended to run on producer nodes. The
-producer noder will be responsible for capturing and
+producer node will be responsible for capturing and
 submission of data onto the Kafka Broker.
 """
 #
@@ -18,7 +19,7 @@ file_segment_time_span = 30                 # File recording segment size
 file_extension = "wav"                      # File recording extension to save the file
 file_quality = "worst"                      # Video quality to record stream at
 kafka_connection_strings=["127.0.0.1:9092"] # Connection strings used to connect to a number of Kafka Brokers
-kafka_topic = 'video'                       # Kafka topic which this produces will subsribe to
+kafka_topic = 'video'                       # Kafka topic which this produces will subscribe to
 exception_time_out = 10                     # Time in seconds to delay process in the case of a timeout
 #
 print("Initiating producer runner..")
@@ -45,8 +46,19 @@ while True:
         #
         # Copies the recorded file into memory, as a collection of binary data
         # video = ri.get_video(video_path=video_path)
-        cloud_url = None  # THIS NEEDS TO BE REPLACED WITH THE RETURN HTTP URL FOR THE CLOUD STORED VIDEO
-        #
+
+        cloud_url_tuple = None
+
+        # NOTE: - This is a synchronous call. Execution will be held until operation
+        #         is complete.
+        #       - BDAGoogleStorageConvertUpload may raise a ValueError exception
+        gs_convert_upload = BDAGoogleStorageConvertUpload(video_path)
+        # cloud_url_tuple[0] --> BucketName
+        # cloud_url_tuple[1] --> BlobPath
+        cloud_url_tuple = gs_convert_upload.upload_file()
+
+        # [Nik]: - Consider deleting the video file at this point? --> os.remove(video_path)
+
         # Prepares the message to be submitted over to Kafka, by creating an object of type stream_object
         config_obj = ci.get_input_channels()[stream_offset].get_details()
         stream_object = StreamObject(platform=config_obj['platform'],
@@ -55,7 +67,7 @@ while True:
                                      genre=config_obj['genre'],
                                      time_stamp=time.ctime(),
                                      file_path=video_path,
-                                     cloud_url=cloud_url,
+                                     cloud_url=cloud_url_tuple,
                                      file=None)
         #
         # Submits message to Kafka broker
