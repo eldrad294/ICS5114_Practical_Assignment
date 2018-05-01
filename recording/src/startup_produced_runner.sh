adca3676ef5f196337c0d17826b7b9d6f47e4981
Unknown <niki.frendo@gmail.com>
2018-05-01 15:30:46 +0200

    Startup script changes

--- recording/src/startup_produced_runner.sh
+++ recording/src/startup_produced_runner.sh
@@ -3,11 +3,4 @@
 cd /root/src/ICS5114_Practical_Assignment
 git pull
 
-cd /root
-source .bashrc
-
-printf "\n\n\n-----------------------"
-echo $PYTHONPATH
-printf "-----------------------\n\n\n"
-
 /usr/bin/python3 /root/src/ICS5114_Practical_Assignment/recording/src/producer/producer_runner.py
