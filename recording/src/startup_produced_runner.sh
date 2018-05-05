26d8fbc494c06e4cb2bcb53ef63fbad5dcc8dd32
Unknown <niki.frendo@gmail.com>
2018-05-05 20:03:06 +0200

    Renamed script

--- recording/src/startup_produced_runner.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /root/src/ICS5114_Practical_Assignment
-git pull
-
-/usr/bin/python3 /root/src/ICS5114_Practical_Assignment/recording/src/producer/producer_runner.py &> producer.out.log
