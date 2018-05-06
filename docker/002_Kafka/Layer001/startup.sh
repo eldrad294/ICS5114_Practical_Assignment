a1bc14a3727acfbce1285fda2fd6c5d9132a3e78
Unknown <niki.frendo@gmail.com>
2018-05-06 18:17:17 +0200

    Docker: Working producer and kafka images
    - Tested in separate docker containers

--- docker/002_Kafka/Layer001/startup.sh
+++ docker/002_Kafka/Layer001/startup.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
+cp -f /root/kafka.server.properties.template /root/kafka.server.properties
+
+# Network interface should be called eth0
+ipAddr=$(ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
+
+listenerConfig="listeners=PLAINTEXT://$ipAddr:9092"
+echo $listenerConfig >> /root/kafka.server.properties
+mv -f /root/kafka.server.properties /opt/kafka_2.11-1.1.0/config/
+
+# Start zookeeper server
 /bin/bash /opt/zookeeper-3.4.12/bin/zkServer.sh start
-/bin/bash /opt/kafka_2.11-1.1.0/bin/kafka-server-start.sh /opt/kafka_2.11-1.1.0/config/server.properties
+
+# Start kafka server
+# Note: Triggering the kafka server scripts holds control of the terminal session. Therefore, the kafka script
+#       is forked and called as a separate OS process. Control is required as we need to create kafka topics
+#       after starting the server.
+mkdir -p /tmp/bda_kafka_logs
+/bin/bash /opt/kafka_2.11-1.1.0/bin/kafka-server-start.sh /opt/kafka_2.11-1.1.0/config/kafka.server.properties > /tmp/bda_kafka_logs/kafka.logs &
+
+# Ensure kafka server is up before progressing further
+while true
+do
+  sleep 10
+
+  kafkaProc="$(jps)"
+  if echo "$kafkaProc" | grep -q "QuorumPeerMain"; then
+    break
+  fi
+done
+
+# Kafka server up, create kafka topic/s
+/opt/kafka_2.11-1.1.0/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic video
+
+# Read kafka server output and keep OS process in memory
+tail -f /tmp/bda_kafka_logs/kafka.logs
---
+++ docker/buildDockerImages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get absolute script directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+cd $DIR'/000_Base'
+docker build -t bda/base_image:1.0 .
+
+cd $DIR'/001_Producer'
+docker build -t bda/producer:1.0 .
+
+cd $DIR'/002_Kafka/Layer001'
+docker build -t bda/kafka:1.0 .
+
+cd $DIR'/002_Kafka/Layer002'
+docker build -t bda/kafka:1.1 .
+
