2da890a8b5ef35ed949af74f7b9b51b3feea97b9
Gabriel Sammut <eldrad294@hotmail.com>
2018-05-08 22:58:45 +0200

    Added working prototype for YouTube API Script for Comment Scraping

--- prototype/text_scraping/youtube_comment_scraper.py
+++ prototype/text_scraping/youtube_comment_scraper.py
@@ -1,15 +1,36 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import time
+#
+def scrollDown(pause, driver):
+    """
+    Function to scroll down till end of page.
+    """
+    iterations, iteration = 10, 1
+    while True:
+        driver.execute_script("window.scrollTo(1, 500);")
+        time.sleep(pause)
+        iteration += 1
+        if iteration > iterations:
+            return driver
 
+# Main Code
 driver = webdriver.Firefox()
-driver.get('https://www.youtube.com/watch?v=6ValJMOpt7s&t=14117s')
-driver.execute_script('window.scrollTo(1,500);')
 
-for i in range(10):
-    driver.execute_script('window.scrollTo(1,3000);')
-    time.sleep(5)
+# Instantiate browser and navigate to page
 
-comment_div=driver.find_element_by_xpath('//*[@id="contents"]')
-comments=comment_div.find_elements_by_xpath('//*[@id="content-text"]')
+driver.get('https://www.youtube.com/watch?v=iFPMz36std4')
+driver = scrollDown(3, driver)
+
+# Page soup
+soup = BeautifulSoup(driver.page_source, "html.parser")
+
+driver.close()
+
+comments = soup.findAll(attrs={'id':'comment','class':"style-scope ytd-comment-thread-renderer"})
+#print(comments)
+i = 0
 for comment in comments:
-    print(comment.text)
+    print("".join(comment.text.split()) + "\n------------------")
+    i += 1
+print(i)
