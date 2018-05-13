69d90c6794fdb30920dac20ea0f8cf52df149463
Unknown <niki.frendo@gmail.com>
2018-05-13 18:08:01 +0200

    Docker - Image build script

--- docker/buildDockerImages.sh
+++ docker/buildDockerImages.sh
@@ -15,15 +15,22 @@ docker build -t bda/base_image:1.0 .
 
 cd $DIR'/001_Producer'
 docker build -t bda/producer:1.0 .
+docker tag bda/producer:1.0 nikifrendo/bda_producer:1.0
 
 cd $DIR'/002_Kafka/Layer001'
 docker build -t bda/kafka:1.0 .
 
 cd $DIR'/002_Kafka/Layer002'
 docker build -t bda/kafka:1.1 .
+docker tag bda/kafka:1.1 nikifrendo/bda_kafka:1.1
 
 cd $DIR'/003_Storm/Layer001'
 docker build -t bda/storm:1.0 .
 
 cd $DIR'/003_Storm/Layer002'
 docker build -t bda/storm:1.1 .
+docker tag bda/storm:1.1 nikifrendo/bda_storm:1.1
+
+cd $DIR'/004_Neo4j'
+docker build -t bda/neo4j:1.0 .
+docker tag bda/neo4j:1.0 nikifrendo/neo4j:1.0
