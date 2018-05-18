3c9692cc009e7b44196c1495aa5164512b86734e
Unknown <niki.frendo@gmail.com>
2018-05-18 12:52:47 +0200

    Automation scripts
    VirtualBox VM creation --> Increased memory allocation to 3GB

--- docker/005_VMAutomation/VirtualBox/VMCreate.sh
+++ docker/005_VMAutomation/VirtualBox/VMCreate.sh
@@ -2,7 +2,7 @@
 
 for i in `seq 1 $2`; do
     vmName=`printf virtualbox-$1-%03d $i`
-    docker-machine create --driver virtualbox $vmName
+    docker-machine create --driver virtualbox --virtualbox-memory 3072 $vmName
     docker-machine ssh $vmName "docker pull $3"
     docker-machine ssh $vmName "tce-setdrive"
     docker-machine ssh $vmName "tce-load -wi nmap"
